f7384b2e2dfce95c128bc19939fe9386fe274565
ericsantos <eric@exactaworks.com.br>
2020-11-26 16:58:03 -0300

    feat: config theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
 
 import GlobalStyles from 'styles/global';
+import theme from 'styles/theme';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <link rel="shortcut icon" href="/img/icon-192.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
@@ -21,7 +23,7 @@ function App({ Component, pageProps }: AppProps) {
 
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
