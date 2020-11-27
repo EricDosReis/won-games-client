23c1d43c21e2695d72c7c0e6d41b76d6f790a1f8
ericsantos <eric@exactaworks.com.br>
2020-11-27 11:55:06 -0300

    feat: create Logo component

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,14 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts',
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
   moduleNameMapper: {
     '^styled-components':
       '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js',
---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,39 @@
+import styled, { css } from 'styled-components';
+import media from 'styled-media-query';
+
+import { LogoProps } from '.';
+
+const wrapperModifiers = {
+  normal: () => css`
+    height: 3.3rem;
+    width: 11rem;
+  `,
+  large: () => css`
+    height: 5.9rem;
+    width: 20rem;
+  `,
+  hideOnMobile: () => css`
+    ${media.lessThan('medium')`
+      height: 4.5rem;
+      width: 5.8rem;
+
+      svg {
+        height: 4.5rem;
+        pointer-events: none;
+      }
+
+      .text {
+        display: none;
+      }
+    `}
+  `,
+};
+
+export const Wrapper = styled.div<LogoProps>`
+  ${({ theme, color, size, hideOnMobile }) => css`
+    color: ${theme.colors[color!]};
+
+    ${!!size && wrapperModifiers[size]};
+    ${!!hideOnMobile && wrapperModifiers.hideOnMobile};
+  `}
+`;
