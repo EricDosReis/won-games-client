03ce0ef04ebe1e549f3e36ce0c9ffa015ab0df89
ericsantos <eric@exactaworks.com.br>
2020-12-23 11:44:37 -0300

    refactor: Container component

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,11 +1,13 @@
-import * as S from './styles';
+import styled, { css } from 'styled-components';
 
-type ContainerProps = {
-  children: React.ReactNode;
-};
-
-const Container = ({ children }: ContainerProps) => (
-  <S.Wrapper>{children}</S.Wrapper>
-);
+const Container = styled.div`
+  ${({ theme }) => css`
+    margin-right: auto;
+    margin-left: auto;
+    max-width: ${theme.grid.container};
+    padding-right: calc(${theme.grid.gutter} / 2);
+    padding-left: calc(${theme.grid.gutter} / 2);
+  `}
+`;
 
 export default Container;
