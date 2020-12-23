ac283806624c759e47e04914e88c13a4b1b2a00e
ericsantos <eric@exactaworks.com.br>
2020-12-23 11:45:23 -0300

    chore: ignore tests in d.ts files

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
     '!src/**/mock.ts',
     '!src/pages/**/*.tsx',
     '!src/styles/**/*.ts',
+    '!src/types/**/*.ts',
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
