be566478ad7320f51047cab3ec2c2fd1b6297665
ericsantos <eric@exactaworks.com.br>
2020-12-30 16:19:22 -0300

    fix: Logo link

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -27,7 +27,9 @@ const Menu = ({ username }: MenuProps) => {
 
       <S.LogoWrapper>
         <Link href="/" passHref>
-          <Logo hideOnMobile />
+          <a>
+            <Logo hideOnMobile />
+          </a>
         </Link>
       </S.LogoWrapper>
 
