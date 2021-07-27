9b50e56b68ca7f65bf13553a313d91fe8c360366
ericsantos <eric@exactaworks.com.br>
2021-07-27 16:39:45 -0300

    feat: fetch games data from api

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,16 +1,31 @@
 import Games, { GamesTemplateProps } from 'templates/Games';
-
 import filterItemsMock from 'components/ExploreSidebar/mock';
-import gamesMock from 'components/GameCardSlider/mock';
+import { initializeApollo } from 'utils/apollo';
+import { formatCurrency } from 'utils/currency';
+import { QUERY_GAMES } from 'graphql/queries/games';
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames';
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <Games {...props} />;
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo();
+
+  const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({
+    query: QUERY_GAMES,
+    variables: { limit: 9 },
+  });
+
   return {
     props: {
-      games: gamesMock,
+      games: data.games.map(({ name, developers, cover, price, slug }) => ({
+        title: name,
+        developer: developers[0].name,
+        img: `http://localhost:1337${cover!.url}`,
+        price: formatCurrency(price),
+        slug,
+      })),
       filterItems: filterItemsMock,
     },
   };
